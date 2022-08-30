7fc172d43a6085ca64d8f1cdff52e6ab52f9df62
Luigi Russo <lrusso@sysmanagement.it>
2022-08-30 16:08:19 +0200

    scadenza

--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -8,7 +8,7 @@ export class WeatherService {
 
   constructor(private http:HttpClient ) { }
 
-  apiKey: string='93f4e71f914c56fb0bdfb46a5952fed4'
+  apiKey: string='93f4e71f914c56fb0bdfb46a5952fed4' //La chiave potrebbe avere una scadenza 
 
   getCurrentWeather(lat:any, lon:any){
     return this.http.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${this.apiKey}`)
